57fc684bb11c0e09a4bc4574003c643539a29e12
Lorenzo de Avila <deavilacarpes@gmail.com>
2026-06-15 18:03:13 -0300

    Remove listar-cartas component and firebase config

--- src/app/pages/cartas/cartas/listar-cartas/listar-cartas.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-listar-cartas',
-  imports: [],
-  templateUrl: './listar-cartas.html',
-  styleUrl: './listar-cartas.scss',
-})
-export class ListarCartas {}
